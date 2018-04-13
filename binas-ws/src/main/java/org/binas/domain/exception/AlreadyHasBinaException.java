550d611c2467f235337039e4f78b00d279855ba3
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2018-04-13 17:34:38 +0100

    Documentaion

--- binas-ws/src/main/java/org/binas/domain/exception/AlreadyHasBinaException.java
+++ binas-ws/src/main/java/org/binas/domain/exception/AlreadyHasBinaException.java
@@ -1,6 +1,6 @@
 package org.binas.domain.exception;
 
-/** Exception used to signal a problem while initializing a station. */
+/** Exception used to signal that the user already has a bina. */
 public class AlreadyHasBinaException extends Exception {
 	private static final long serialVersionUID = 1L;
 
--- binas-ws/src/main/java/org/binas/domain/exception/FullStationException.java
+++ binas-ws/src/main/java/org/binas/domain/exception/FullStationException.java
@@ -1,6 +1,6 @@
 package org.binas.domain.exception;
 
-/** Exception used to signal that no slots are are currently available in a station. */
+/** Exception used to signal that the station is full and has no slots available. */
 public class FullStationException extends Exception {
 	private static final long serialVersionUID = 1L;
 
